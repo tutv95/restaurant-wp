61649b4b77c40f2c1a0a838a186194085becff11
Tu TV <tutv95@gmail.com>
2015-12-07 20:27:32 +0700

    Updated js

--- functions.php
+++ functions.php
@@ -116,9 +116,9 @@ add_action( 'widgets_init', 'restaurant_wp_widgets_init' );
 function restaurant_wp_scripts() {
 	wp_enqueue_style( 'restaurant-wp-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'restaurant-wp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'restaurant-wp-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'restaurant-wp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'restaurant-wp-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
