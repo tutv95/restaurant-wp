74736a0029b47f54d394500a69884e135f5ae303
Tu TV <tutv95@gmail.com>
2015-12-07 19:52:15 +0700

    __

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -5,3 +5,13 @@
  * Date: 07/12/2015
  * Time: 7:38 PM
  */
+
+
+/**
+ * Rewrite uri stylesheet
+ */
+function seo_wp_rewrite_uri_stylesheet() {
+	return get_stylesheet_directory_uri() . '/style.min.css?time=' . md5( time() );
+}
+
+add_action( 'stylesheet_uri', 'seo_wp_rewrite_uri_stylesheet' );
