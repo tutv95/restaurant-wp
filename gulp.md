97d06c81c59b1d2bfa3d0b585c994733ad7dfc30
Tu TV <tutv95@gmail.com>
2015-12-07 19:20:11 +0700

    Add bootstrap

---
+++ gulp.md
@@ -0,0 +1,5 @@
+npm install --global gulp
+
+npm install --save-dev gulp
+
+npm install --save-dev gulp-jshint gulp-sass gulp-concat gulp-uglify gulp-rename gulp-livereload gulp-minify-css
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var livereload = require('gulp-livereload');
+var minifyCss = require('gulp-minify-css');
+
+// Compile Our Sass
+gulp.task('sass', function () {
+	return gulp.src('assets/sass/style.scss')
+		.pipe(sass())
+		.pipe(minifyCss({compatibility: 'ie8'}))
+		.pipe(rename('style.min.css'))
+		.pipe(gulp.dest(''))
+		.pipe(livereload());
+});
+
+// Watch Files For Changes
+gulp.task('watch', function () {
+	livereload.listen();
+	//gulp.watch('assets/js/*.js', ['scripts']);
+	gulp.watch(['assets/sass/**/*'], ['sass']);
+});
+
+// Default Task
+//gulp.task('default', ['sass', 'scripts', 'watch']);
+gulp.task('default', ['sass', 'watch']);
