ca48b0079b81688485352b9c8c541df553873ca0
Tu TV <tutv95@gmail.com>
2015-12-12 15:05:11 +0700

    Updated layout archvie

--- archive.php
+++ archive.php
@@ -2,50 +2,56 @@
 /**
  * The template for displaying archive pages.
  *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * @link    https://codex.wordpress.org/Template_Hierarchy
  *
  * @package Restaurant_WP
  */
 
 get_header(); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9">
+				<div id="primary" class="content-area">
+					<main id="main" class="site-main" role="main">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+						<?php
+						if ( have_posts() ) : ?>
 
-		<?php
-		if ( have_posts() ) : ?>
+							<header class="page-header">
+								<?php
+								the_archive_title( '<h1 class="page-title">', '</h1>' );
+								the_archive_description( '<div class="taxonomy-description">', '</div>' );
+								?>
+							</header><!-- .page-header -->
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+							<?php
+							/* Start the Loop */
+							while ( have_posts() ) : the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+								/*
+								 * Include the Post-Format-specific template for the content.
+								 * If you want to override this in a child theme, then include a file
+								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+								 */
+								get_template_part( 'template-parts/content', get_post_format() );
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+							endwhile;
 
-			endwhile;
+							the_posts_navigation();
 
-			the_posts_navigation();
+						else :
 
-		else :
+							get_template_part( 'template-parts/content', 'none' );
 
-			get_template_part( 'template-parts/content', 'none' );
+						endif; ?>
 
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+					</main><!-- #main -->
+				</div><!-- #primary -->
+			</div>
+			<div class="col-md-3">
+				<?php get_sidebar(); ?>
+			</div>
+		</div>
+	</div>
 <?php
-get_sidebar();
 get_footer();
