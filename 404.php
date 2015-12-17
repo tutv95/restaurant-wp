9c0ef4a46cf2d1becb05726dff30c611bc3736eb
Tu TV <tutv95@gmail.com>
2015-12-17 17:49:51 +0700

    Updated main top

--- 404.php
+++ 404.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 						<section class="error-404 not-found">
 							<header class="page-header">
-								<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'restaurant-wp' ); ?></h1>
+								<h2 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'restaurant-wp' ); ?></h2>
 							</header><!-- .page-header -->
 
 							<div class="page-content">
--- search.php
+++ search.php
@@ -18,8 +18,8 @@ get_header(); ?>
 		if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'restaurant-wp' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+				<h2 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'restaurant-wp' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
+			</header><!-- .page-header
 
 			<?php
 			/* Start the Loop */
