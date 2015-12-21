a2cde1a3b2a9f5708e5660ab85156c63c07cc982
Tu TV <tutv95@gmail.com>
2015-12-21 16:29:20 +0700

    Updated restaurant menu widget

--- inc/siteorigin-widgets/restaurant-menu/restaurant-menu.php
+++ inc/siteorigin-widgets/restaurant-menu/restaurant-menu.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Widget Name: Restaurant Menu
+ * Widget Name: WPArena - Restaurant Menu
  * Description: Widget for Restaurant Menu
  * Author: WPArena
  * Author URI: wparena.com
@@ -12,264 +12,82 @@
  */
 class RESWP_Restaurant_Menu_Widget extends SiteOrigin_Widget {
 	function __construct() {
+		$erm_menu_args = array(
+			'post_type'      => 'erm_menu',
+			'posts_per_page' => - 1,
+		);
+		$lop_menu_args = new WP_Query( $erm_menu_args );
+		$cate[0]       = esc_html__( 'Create Menu', 'restaurant-wp' );
+		if ( $lop_menu_args->have_posts() ) {
+			$cate = '';
+			while ( $lop_menu_args->have_posts() ): $lop_menu_args->the_post();
+				$cate[get_the_ID()] = get_the_title( get_the_ID() );;
+			endwhile;
+		}
+		wp_reset_postdata();
+
 		$form_options = array(
-			'some_tinymce_editor'     => array(
-				'type'           => 'tinymce',
-				'label'          => __( 'Visually edit, richly.', 'restaurant-wp' ),
-				'default'        => 'An example of a long message.</br>It is even possible to add a few html tags.</br><a href="siteorigin.com" target="_blank"">Links!</a>',
-				'rows'           => 10,
-				'default_editor' => 'html',
-				'button_filters' => array(
-					'mce_buttons'        => array( $this, 'filter_mce_buttons' ),
-					'mce_buttons_2'      => array( $this, 'filter_mce_buttons_2' ),
-					'mce_buttons_3'      => array( $this, 'filter_mce_buttons_3' ),
-					'mce_buttons_4'      => array( $this, 'filter_mce_buttons_5' ),
-					'quicktags_settings' => array( $this, 'filter_quicktags_settings' ),
-				),
+			'header_type'       => array(
+				'type'    => 'select',
+				'label'   => esc_html__( 'Type', 'restaurant-wp' ),
+				'default' => 'h3',
+				'options' => array(
+					'h2' => esc_html__( 'h2', 'restaurant-wp' ),
+					'h3' => esc_html__( 'h3', 'restaurant-wp' ),
+					'h4' => esc_html__( 'h4', 'restaurant-wp' ),
+					'h5' => esc_html__( 'h5', 'restaurant-wp' ),
+					'h6' => esc_html__( 'h6', 'restaurant-wp' ),
+				)
 			),
-			'some_text'               => array(
-				'type'    => 'text',
-				'label'   => __( 'Some text goes here', 'restaurant-wp' ),
-				'default' => 'Some default text.'
-			),
-			'some_url'                => array(
-				'type'    => 'link',
-				'label'   => __( 'Some URL goes here', 'restaurant-wp' ),
-				'default' => 'http://www.example.com'
-			),
-			'some_color'              => array(
+			'header_color'      => array(
 				'type'    => 'color',
-				'label'   => __( 'Choose a color', 'restaurant-wp' ),
-				'default' => '#bada55'
+				'label'   => esc_html__( 'Choose a color', 'restaurant-wp' ),
+				'default' => '#fff'
 			),
-			'some_number'             => array(
-				'type'    => 'number',
-				'label'   => __( 'Enter a number', 'restaurant-wp' ),
-				'default' => '12654'
-			),
-			'some_long_message'       => array(
-				'type'    => 'textarea',
-				'label'   => __( 'Type a message', 'restaurant-wp' ),
-				'default' => 'An example of a long message.</br>It is even possible to add a few html tags.</br><a href="siteorigin.com" target="_blank"">Links!</a></br><strong>Strong</strong> and <em>emphasized</em> text.',
-				'rows'    => 5
-			),
-			'some_number_in_a_range'  => array(
-				'type'    => 'slider',
-				'label'   => __( 'Choose a number', 'restaurant-wp' ),
-				'default' => 24,
-				'min'     => 0,
-				'max'     => 42
-			),
-			'some_selection'          => array(
-				'type'    => 'select',
-				'label'   => __( 'Choose a thing from a long list of things', 'restaurant-wp' ),
-				'default' => 'the_other_thing',
-				'options' => array(
-					'this_thing'      => __( 'This thing', 'restaurant-wp' ),
-					'that_thing'      => __( 'That thing', 'restaurant-wp' ),
-					'the_other_thing' => __( 'The other thing', 'restaurant-wp' ),
-					'thing_1'         => __( 'Thing One', 'restaurant-wp' ),
-					'thing_2'         => __( 'Thing Two', 'restaurant-wp' ),
-					'thing_3'         => __( 'Thing Three', 'restaurant-wp' ),
-					'thing_4'         => __( 'Thing Four', 'restaurant-wp' ),
-					'thing_5'         => __( 'Thing Five', 'restaurant-wp' ),
-					'thing_6'         => __( 'Thing Six', 'restaurant-wp' ),
-					'thing_7'         => __( 'Thing Seven', 'restaurant-wp' ),
-					'thing_8'         => __( 'Thing Eight', 'restaurant-wp' ),
-					'thing_9'         => __( 'Thing Nine', 'restaurant-wp' ),
-					'thing_10'        => __( 'Thing Ten', 'restaurant-wp' ),
-				)
-			),
-			'some_multiple_selection' => array(
-				'type'     => 'select',
-				'label'    => __( 'Choose many things from a long list of things', 'restaurant-wp' ),
-				'multiple' => true,
-				'default'  => 'the_other_thing',
-				'options'  => array(
-					'this_thing'      => __( 'This thing', 'restaurant-wp' ),
-					'that_thing'      => __( 'That thing', 'restaurant-wp' ),
-					'the_other_thing' => __( 'The other thing', 'restaurant-wp' ),
-					'thing_1'         => __( 'Thing One', 'restaurant-wp' ),
-					'thing_2'         => __( 'Thing Two', 'restaurant-wp' ),
-					'thing_3'         => __( 'Thing Three', 'restaurant-wp' ),
-					'thing_4'         => __( 'Thing Four', 'restaurant-wp' ),
-					'thing_5'         => __( 'Thing Five', 'restaurant-wp' ),
-					'thing_6'         => __( 'Thing Six', 'restaurant-wp' ),
-					'thing_7'         => __( 'Thing Seven', 'restaurant-wp' ),
-					'thing_8'         => __( 'Thing Eight', 'restaurant-wp' ),
-					'thing_9'         => __( 'Thing Nine', 'restaurant-wp' ),
-					'thing_10'        => __( 'Thing Ten', 'restaurant-wp' ),
-				)
-			),
-			'another_selection'       => array(
-				'type'    => 'select',
-				'prompt'  => __( 'Choose a thing from a long list of things', 'restaurant-wp' ),
-				'options' => array(
-					'this_thing'      => __( 'This thing', 'restaurant-wp' ),
-					'that_thing'      => __( 'That thing', 'restaurant-wp' ),
-					'the_other_thing' => __( 'The other thing', 'restaurant-wp' ),
-					'thing_1'         => __( 'Thing One', 'restaurant-wp' ),
-					'thing_2'         => __( 'Thing Two', 'restaurant-wp' ),
-					'thing_3'         => __( 'Thing Three', 'restaurant-wp' ),
-					'thing_4'         => __( 'Thing Four', 'restaurant-wp' ),
-					'thing_5'         => __( 'Thing Five', 'restaurant-wp' ),
-					'thing_6'         => __( 'Thing Six', 'restaurant-wp' ),
-					'thing_7'         => __( 'Thing Seven', 'restaurant-wp' ),
-					'thing_8'         => __( 'Thing Eight', 'restaurant-wp' ),
-					'thing_9'         => __( 'Thing Nine', 'restaurant-wp' ),
-					'thing_10'        => __( 'Thing Ten', 'restaurant-wp' ),
-				)
-			),
-			'some_boolean'            => array(
-				'type'    => 'checkbox',
-				'label'   => __( 'Allow this thing?', 'restaurant-wp' ),
-				'default' => true
-			),
-			'radio_selection'         => array(
-				'type'    => 'radio',
-				'label'   => __( 'Choose a thing from a short list of things', 'restaurant-wp' ),
-				'default' => 'that_thing',
-				'options' => array(
-					'this_thing'      => __( 'This thing', 'restaurant-wp' ),
-					'that_thing'      => __( 'That thing', 'restaurant-wp' ),
-					'the_other_thing' => __( 'The other thing', 'restaurant-wp' )
-				)
-			),
-			'some_media'              => array(
+			'header_background' => array(
 				'type'     => 'media',
-				'label'    => __( 'Choose a media thing', 'restaurant-wp' ),
-				'choose'   => __( 'Choose image', 'restaurant-wp' ),
-				'update'   => __( 'Set image', 'restaurant-wp' ),
-				'library'  => 'image',//'image', 'audio', 'video', 'file'
-				'fallback' => true
+				'label'    => esc_html__( 'Background image for header', 'restaurant-wp' ),
+				'choose'   => esc_html__( 'Choose image', 'restaurant-wp' ),
+				'update'   => esc_html__( 'Set image', 'restaurant-wp' ),
+				'library'  => 'image',
+				'fallback' => false,
 			),
-			'some_posts'              => array(
-				'type'  => 'posts',
-				'label' => __( 'Some posts query', 'restaurant-wp' ),
+			'quick_menu'        => array(
+				'type'    => 'select',
+				'label'   => esc_html__( 'Choose a menu.', 'restaurant-wp' ),
+				'default' => 0,
+				'options' => $cate
 			),
-			'a_section'               => array(
-				'type'   => 'section',
-				'label'  => __( 'A section containing related fields.', 'restaurant-wp' ),
-				'hide'   => true,
-				'fields' => array(
-					'grouped_text'     => array(
-						'type'  => 'text',
-						'label' => __( 'A grouped text field', 'restaurant-wp' )
-					),
-					'grouped_checkbox' => array(
-						'type'  => 'checkbox',
-						'label' => __( 'A grouped checkbox', 'restaurant-wp' )
-					)
+			'columns'           => array(
+				'type'    => 'select',
+				'label'   => esc_html__( 'Columns', 'restaurant-wp' ),
+				'default' => 1,
+				'options' => array(
+					'1' => 1,
+					'2' => 2,
 				)
 			),
-			'a_repeater'              => array(
-				'type'         => 'repeater',
-				'label'        => __( 'A repeating repeater.', 'restaurant-wp' ),
-				'item_name'    => __( 'Repeater item', 'siteorigin-widgets' ),
-				'scroll_count' => 10,
-				'item_label'   => array(
-					'selector'     => "[id*='repeat_text']",
-					'update_event' => 'change',
-					'value_method' => 'val'
-				),
-				'fields'       => array(
-					'repeat_text'     => array(
-						'type'  => 'text',
-						'label' => __( 'A text field in a repeater item.', 'restaurant-wp' )
-					),
-					'repeat_checkbox' => array(
-						'type'  => 'checkbox',
-						'label' => __( 'A checkbox in a repeater item.', 'restaurant-wp' )
-					)
+			'menu_style'        => array(
+				'type'    => 'select',
+				'label'   => esc_html__( 'Menu style', 'restaurant-wp' ),
+				'default' => 'regular',
+				'options' => array(
+					'regular' => esc_html__( 'Regular', 'restaurant-wp' ),
+					'dotted'  => esc_html__( 'Dotted', 'restaurant-wp' ),
 				)
-			),
-			'some_widget'             => array(
-				'type'  => 'widget',
-				'label' => __( 'Button Widget', 'restaurant-wp' ),
-				'class' => 'SiteOrigin_Widget_Button_Widget',
-				'hide'  => true
-			),
-			'some_icon'               => array(
-				'type'  => 'icon',
-				'label' => __( 'Select an icon', 'restaurant-wp' ),
-			),
-			'some_font'               => array(
-				'type'  => 'font',
-				'label' => __( 'Select a font', 'restaurant-wp' ),
-			),
-			'some_date'               => array(
-				'type'     => 'text',
-				'label'    => __( 'Some date goes here', 'restaurant-wp' ),
-				'sanitize' => 'date',
 			),
 		);
 
-		add_filter( 'siteorigin_widgets_sanitize_field_date', array( $this, 'sanitize_date' ) );
-
 		parent::__construct(
 			'reswp_restaurant_menu_widget',
-			__( 'Restaurant Menu', 'restaurant-wp' ),
+			esc_html__( 'WPArena: Restaurant Menu', 'restaurant-wp' ),
 			array(
-				'description' => __( 'A blank widget which simply demonstrates the use of all the widget admin form fields.', 'restaurant-wp' ),
+				'description' => esc_html__( 'Widget for Quick Restaurant Menu', 'restaurant-wp' ),
 			),
 			array(),
 			$form_options,
 			plugin_dir_path( __FILE__ )
 		);
-	}
-
-	function filter_mce_buttons( $buttons, $editor_id ) {
-		if ( ( $key = array_search( 'fullscreen', $buttons ) ) !== false ||
-			( $key = array_search( 'dfw', $buttons ) ) !== false
-		) {
-			unset( $buttons[$key] );
-		}
-
-		return $buttons;
-	}
-
-	function filter_mce_buttons_2( $buttons, $editor_id ) {
-		if ( ( $key = array_search( 'fullscreen', $buttons ) ) !== false ||
-			( $key = array_search( 'dfw', $buttons ) ) !== false
-		) {
-			unset( $buttons[$key] );
-		}
-
-		return $buttons;
-	}
-
-	function filter_mce_buttons_3( $buttons, $editor_id ) {
-		if ( ( $key = array_search( 'fullscreen', $buttons ) ) !== false ||
-			( $key = array_search( 'dfw', $buttons ) ) !== false
-		) {
-			unset( $buttons[$key] );
-		}
-
-		return $buttons;
-	}
-
-	function filter_mce_buttons_4( $buttons, $editor_id ) {
-		if ( ( $key = array_search( 'fullscreen', $buttons ) ) !== false ||
-			( $key = array_search( 'dfw', $buttons ) ) !== false
-		) {
-			unset( $buttons[$key] );
-		}
-
-		return $buttons;
-	}
-
-	public function quicktags_settings( $settings, $editor_id ) {
-		$settings['buttons'] = preg_replace( '/,fullscreen/', '', $settings['buttons'] );
-		$settings['buttons'] = preg_replace( '/,dfw/', '', $settings['buttons'] );
-
-		return $settings;
-	}
-
-	function sanitize_date( $date_to_sanitize ) {
-		// Perform custom date sanitization here.
-		$sanitized_date = sanitize_text_field( $date_to_sanitize );
-
-		return $sanitized_date;
 	}
 
 	function get_template_name( $instance ) {
@@ -282,6 +100,13 @@ class RESWP_Restaurant_Menu_Widget extends SiteOrigin_Widget {
 
 	function get_style_name( $instance ) {
 		return '';
+	}
+
+	function enqueue_frontend_scripts( $instance ) {
+		parent::enqueue_frontend_scripts( $instance ); // TODO: Change the autogenerated stub
+		wp_enqueue_style( 'magnific-popup', RESWP_THEME_URL . 'inc/siteorigin-widgets/restaurant-menu/css/magnific-popup.css' );
+		wp_enqueue_script( 'magnific-popup', RESWP_THEME_URL . 'inc/siteorigin-widgets/restaurant-menu/js/jquery.magnific-popup.min.js', array( 'jquery' ), '', true );
+		wp_enqueue_script( 'erm-front', RESWP_THEME_URL . 'inc/siteorigin-widgets/restaurant-menu/js/erm-front-scripts.js', array( 'jquery' ), '', true );
 	}
 }
 
--- inc/siteorigin-widgets/restaurant-menu/templates/restaurant-menu.php
+++ inc/siteorigin-widgets/restaurant-menu/templates/restaurant-menu.php
@@ -4,9 +4,124 @@
  * User: Tu TV
  * Date: 21/12/2015
  * Time: 10:20 AM
+ *
+ * @package Restaurant_WP
  */
-?>
 
-<div>
-	<?php echo wp_kses_post( $instance['some_tinymce_editor'] ) ?>
-</div>
+if ( ! isset( $instance['quick_menu'] ) ) {
+	return;
+}
+
+$menu_id = $instance['quick_menu'];
+
+if ( get_post_type( $menu_id ) != 'erm_menu' ) {
+	return;
+}
+
+$columns      = intval( $instance['columns'] );
+$header_type  = $instance['header_type'];
+$header_color = $instance['header_color'];
+$header_title = get_the_title( $menu_id );
+
+$header_background = '';
+if ( isset( $instance['header_background'] ) ) {
+	$header_background = wp_get_attachment_image_src( $instance['header_background'], 'full' );
+}
+$header_background_src = '';
+if ( $header_background != false ) {
+	$header_background_src = $header_background[0];
+}
+
+if ( $header_background_src != '' ) {
+	$style_header = 'background-image: url(\'' . $header_background_src . '\');';
+}
+
+$style_header .= 'color: ' . $header_color . ';';
+
+$title_menu = get_the_title( $menu_id );
+
+// Menu items
+$menu_items = get_post_meta( $menu_id, '_erm_menu_items', true );
+if ( empty( $menu_items ) ) {
+	return;
+}
+$menu_items = preg_split( '/,/', $menu_items );
+?>
+	<div class="erm_menu">
+	<div class="menu-title" style="<?php echo esc_attr( $style_header ); ?>">
+		<?php echo '<' . $header_type . ' class="title">' . esc_html( $header_title ) . '</' . $header_type . '>'; ?>
+	</div>
+	<ul class="erm_menu_content">
+<?php
+
+$args_query = array(
+	'post_type' => 'erm_menu_item',
+	'post__in' => $menu_items,
+	'ignore_sticky_posts' => true,
+);
+$menu_query = new WP_Query( $args_query );
+
+if ( $menu_query->have_posts() ) {
+	while ( $menu_query->have_posts() ) {
+		$menu_query->the_post();
+
+		$item_id = get_the_ID();
+		$visible = get_post_meta( $item_id, '_erm_visible', true );
+
+		if ( $visible != 1 ) {
+			continue;
+		}
+
+		$type_item    = get_post_meta( $item_id, '_erm_type', true );
+
+		if ( $type_item == 'section' ) {
+			?>
+				<li class="erm_section">
+					<h2 class="erm_section_title"><?php echo esc_html( get_the_title() ); ?></h2>
+    				<div class="erm_section_desc"><?php the_content(); ?></div>
+				</li>
+			<?php
+		} else {
+			$has_thumbnail = has_post_thumbnail( $item_id );
+			$prices        = get_post_meta( $item_id, '_erm_prices', true );
+			?>
+				<li class="erm_product <?php echo ( $has_thumbnail ? 'with_image' : 'no_image' ); ?>">
+					<?php if ( $has_thumbnail ) :
+						$image_id   = get_post_thumbnail_id( $item_id );
+						$image_thumb  = wp_get_attachment_image_src( $image_id, 'thumbnail' )[0];
+						$image_full   = wp_get_attachment_image_src( $image_id, 'full' )[0];
+						$post_image = get_post( $image_id );
+						?>
+						<div class="item-left">
+							<a target="_blank" href="<?php echo esc_url( $image_full ); ?>" class="image-popup" data-caption="<?php echo esc_attr( $post_image->post_excerpt ); ?>" data-desc="<?php echo esc_attr( $post_image->post_content ); ?>">
+								<img class="erm_product_image" src="<?php echo esc_url( $image_thumb ); ?>" alt="">
+							</a>
+							<h3 class="erm_product_title"><?php echo esc_html( get_the_title() ); ?></h3>
+							<div class="erm_product_desc"><?php the_content(); ?></div>
+						</div>
+					<?php endif; ?>
+					<?php if ( !empty( $prices ) ) : ?>
+						<div class="erm_product_price">
+							<ul>
+							<?php
+								foreach ($prices as $price) {
+									?>
+									<li>
+										<span class="name"><?php echo esc_html($price['name']); ?></span>
+										<span class="price"><?php echo esc_html($price['value']); ?></span>
+									</li>
+									<?php
+								}
+ 							?>
+							</ul>
+						</div>
+					<?php endif; ?>
+				</li>
+			<?php
+		}
+	}
+}
+
+wp_reset_postdata();
+echo '</ul>';
+echo '</div>';
