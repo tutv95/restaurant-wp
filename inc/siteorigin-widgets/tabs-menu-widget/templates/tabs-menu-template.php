a3f9c887562ffd4f667e77ff1405799fd83ffb7f
daitd58 <neverfrown.95@gmail.com>
2015-12-23 10:09:13 +0700

    update tabs-menu-widget

---
+++ inc/siteorigin-widgets/tabs-menu-widget/templates/tabs-menu-template.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: never
+ * Date: 12/22/2015
+ * Time: 3:56 PM
+ *
+ * @package: Restaurant_WP
+ */
+
+//if( !isset( $instance['tab'] ) ) {
+//	return;
+//}
+
+//$tabs = $instance['tab'];
+//echo 'xxx';
+//echo count( $tabs );
+
+?>
+<div class="reswp_tab_menu">XXX</div>
