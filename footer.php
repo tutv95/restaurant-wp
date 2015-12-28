47089ef60e956b15e1e06a7b7ad24e39ad7cb69d
Tu TV <tutv95@gmail.com>
2015-12-28 16:26:48 +0700

    Updated copyright footer

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'restaurant-wp' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'restaurant-wp' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'restaurant-wp' ), 'restaurant-wp', '<a href="http://wparena.com" rel="author">WordPress Arena</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'restaurant-wp' ), 'Restaurant WP', '<a href="http://wparena.com" rel="author">WordPress Arena</a>' ); ?>
 		</div><!-- .site-info -->
 	</div>
 </footer><!-- #colophon -->
