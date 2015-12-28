2e40b2ddd11cace4d292d09c82a120665445e393
Tu TV <tutv95@gmail.com>
2015-12-28 11:26:53 +0700

    Updated content page

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -22,18 +22,4 @@
 		);
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php
-		edit_post_link(
-			sprintf(
-			/* translators: %s: Name of current post */
-				esc_html__( 'Edit %s', 'restaurant-wp' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			),
-			'<span class="edit-link">',
-			'</span>'
-		);
-		?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
