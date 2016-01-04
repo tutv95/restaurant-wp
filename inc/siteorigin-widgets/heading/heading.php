7531f01eb8385e1a077bb922846a151c02d67ce8
Tu TV <tutv95@gmail.com>
2016-01-04 11:00:08 +0700

    Updated heading widget

--- inc/siteorigin-widgets/heading/heading.php
+++ inc/siteorigin-widgets/heading/heading.php
@@ -27,7 +27,7 @@ class RESWP_Heading_Widget extends SiteOrigin_Widget {
 
 			'heading_size' => array(
 				'type'    => 'select',
-				'label'   => esc_html__( 'Sub Heading', 'restaurant-wp' ),
+				'label'   => esc_html__( 'Size Heading', 'restaurant-wp' ),
 				'options' => array(
 					'h2' => __( 'h2', 'restaurant-wp' ),
 					'h3' => __( 'h3', 'restaurant-wp' ),
@@ -36,6 +36,63 @@ class RESWP_Heading_Widget extends SiteOrigin_Widget {
 					'h6' => __( 'h6', 'restaurant-wp' )
 				),
 				'default' => 'h3'
+			),
+
+			'custom_font_heading' => array(
+				'type'          => 'section',
+				'label'         => __( 'Custom Font Heading', 'restaurant-wp' ),
+				'hide'          => true,
+				'state_handler' => array(
+					'font_heading_type[custom]'  => array( 'show' ),
+					'font_heading_type[default]' => array( 'hide' ),
+				),
+				'fields'        => array(
+					'custom_text_color'  => array(
+						'type'    => 'color',
+						'label'   => __( 'Text Heading color', 'restaurant-wp' ),
+						'default' => '#333',
+					),
+					'custom_font_size'   => array(
+						'type'        => 'number',
+						'label'       => __( 'Font Size', 'restaurant-wp' ),
+						'suffix'      => 'px',
+						'default'     => 50,
+						'description' => __( 'custom font size', 'restaurant-wp' ),
+						'class'       => 'color-mini',
+					),
+					'custom_font_weight' => array(
+						'type'        => 'select',
+						'label'       => __( 'Custom Font Weight', 'restaurant-wp' ),
+						'options'     => array(
+							'normal' => __( 'Normal', 'restaurant-wp' ),
+							'bold'   => __( 'Bold', 'restaurant-wp' ),
+							'100'    => __( '100', 'restaurant-wp' ),
+							'200'    => __( '200', 'restaurant-wp' ),
+							'300'    => __( '300', 'restaurant-wp' ),
+							'400'    => __( '400', 'restaurant-wp' ),
+							'500'    => __( '500', 'restaurant-wp' ),
+							'600'    => __( '600', 'restaurant-wp' ),
+							'700'    => __( '700', 'restaurant-wp' ),
+							'800'    => __( '800', 'restaurant-wp' ),
+							'900'    => __( '900', 'restaurant-wp' )
+						),
+						'description' => __( 'Select Custom Font Weight', 'restaurant-wp' ),
+						'class'       => 'color-mini',
+					),
+					'custom_font_style'  => array(
+						'type'        => 'select',
+						'label'       => __( 'Custom Font Style', 'restaurant-wp' ),
+						'options'     => array(
+							'inherit' => __( 'inherit', 'restaurant-wp' ),
+							'initial' => __( 'initial', 'restaurant-wp' ),
+							'italic'  => __( 'italic', 'restaurant-wp' ),
+							'normal'  => __( 'normal', 'restaurant-wp' ),
+							'oblique' => __( 'oblique', 'restaurant-wp' )
+						),
+						'description' => __( 'Select Custom Font Style', 'restaurant-wp' ),
+						'class'       => 'color-mini',
+					),
+				),
 			),
 		);
 
--- inc/siteorigin-widgets/heading/templates/heading-template.php
+++ inc/siteorigin-widgets/heading/templates/heading-template.php
@@ -1,9 +1,31 @@
+<?php
+
+if ( $instance['custom_font_heading']['custom_text_color'] != '' ) {
+	$css .= 'color:' . $instance['custom_font_heading']['custom_text_color'] . ';';
+}
+if ( $instance['custom_font_heading']['custom_font_size'] != '' ) {
+	$css .= 'font-size:' . $instance['custom_font_heading']['custom_font_size'] . 'px;';
+}
+if ( $instance['custom_font_heading']['custom_font_weight'] != '' ) {
+	$css .= 'font-weight:' . $instance['custom_font_heading']['custom_font_weight'] . ';';
+}
+if ( $instance['custom_font_heading']['custom_font_style'] != '' ) {
+	$css .= 'font-style:' . $instance['custom_font_heading']['custom_font_style'] . ';';
+}
+
+if ( $css ) {
+	$css = ' style="' . $css . '"';
+}
+
+?>
+
+
 <div class="reswp_heading">
 	<?php if ( isset( $instance['sub_title'] ) ) {
 		echo '<div class="sub_heading">' . $instance['sub_title'] . '</div>';
 	} ?>
 
 	<?php if ( isset( $instance['title'] ) ) {
-		printf( '<%1$s class="title_heading">%2$s</%1$s>', $instance['heading_size'], $instance['title'] );
+		printf( '<%1$s class="title_heading" %3$s>%2$s</%1$s>', $instance['heading_size'], $instance['title'], $css );
 	} ?>
 </div>
