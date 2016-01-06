341903e8ee5bf9dd34bf80b7da2ba1e445a54733
Tu TV <tutv95@gmail.com>
2016-01-06 10:19:28 +0700

    Updated heading widget

--- inc/siteorigin-widgets/heading/templates/heading-template.php
+++ inc/siteorigin-widgets/heading/templates/heading-template.php
@@ -1,15 +1,15 @@
 <?php
 
-if ( $instance['custom_font_heading']['custom_text_color'] != '' ) {
+if ( $instance['custom_font_heading']['custom_text_color'] <> '' ) {
 	$css .= 'color:' . $instance['custom_font_heading']['custom_text_color'] . ';';
 }
-if ( $instance['custom_font_heading']['custom_font_size'] != '' ) {
+if ( $instance['custom_font_heading']['custom_font_size'] <> '' ) {
 	$css .= 'font-size:' . $instance['custom_font_heading']['custom_font_size'] . 'px;';
 }
-if ( $instance['custom_font_heading']['custom_font_weight'] != '' ) {
+if ( $instance['custom_font_heading']['custom_font_weight'] <> '' ) {
 	$css .= 'font-weight:' . $instance['custom_font_heading']['custom_font_weight'] . ';';
 }
-if ( $instance['custom_font_heading']['custom_font_style'] != '' ) {
+if ( $instance['custom_font_heading']['custom_font_style'] <> '' ) {
 	$css .= 'font-style:' . $instance['custom_font_heading']['custom_font_style'] . ';';
 }
 
--- inc/siteorigin-widgets/spacing/spacing.php
+++ inc/siteorigin-widgets/spacing/spacing.php
@@ -14,23 +14,23 @@ class RESWP_Spacing_Widget extends SiteOrigin_Widget {
 	function __construct() {
 		$form_options = array(
 			'height' => array(
-				'label' => esc_html__( 'Height', 'restaurant-wp' ),
-				'type'  => 'number',
-				'default'=>'30',
-				'desc'  => __( "Enter empty space height.", "restaurant-wp" ),
-				'suffix'     => 'px',
+				'label'   => esc_html__( 'Height', 'restaurant-wp' ),
+				'type'    => 'number',
+				'default' => '30',
+				'desc'    => __( 'Enter empty space height.', 'restaurant-wp' ),
+				'suffix'  => 'px',
 			)
 		);
 
 		parent::__construct(
-				'reswp_spacing_widget',
-				esc_html__( 'WPArena: Spacing widget', 'restaurant-wp'),
-				array(
-						'description' => esc_html__( 'Widget for Spacing', 'restaurant-wp' )
-				),
-				array(),
-				$form_options,
-				plugin_dir_path( __FILE__ )
+			'reswp_spacing_widget',
+			esc_html__( 'WPArena: Spacing widget', 'restaurant-wp' ),
+			array(
+				'description' => esc_html__( 'Widget for Spacing', 'restaurant-wp' )
+			),
+			array(),
+			$form_options,
+			plugin_dir_path( __FILE__ )
 		);
 	}
 
@@ -44,13 +44,6 @@ class RESWP_Spacing_Widget extends SiteOrigin_Widget {
 
 	function get_style_name( $instance ) {
 		return '';
-	}
-
-	function enqueue_frontend_scripts( $instance ) {
-		parent::enqueue_frontend_scripts( $instance );
-		wp_enqueue_style( 'magnific-popup', RESWP_THEME_URL . 'inc/siteorigin-widgets/assets/css/magnific-popup.css' );
-		wp_enqueue_script( 'magnific-popup', RESWP_THEME_URL . 'inc/siteorigin-widgets/assets/js/jquery.magnific-popup.min.js', array( 'jquery' ), '1.0.0', true );
-		wp_enqueue_script( 'erm-front', RESWP_THEME_URL . 'inc/siteorigin-widgets/assets/js/erm-front-scripts.js', array( 'jquery' ), '', true );
 	}
 }
 
